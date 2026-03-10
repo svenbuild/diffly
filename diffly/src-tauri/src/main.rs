9ad1763166a4fdea5d2aebaa9b64fc05f7d739ef
Pache Sven <Sven.Pache@veratron.com>
2026-03-10 11:26:54 +0100

    Fix Diffly configuration UI

--- diffly/src-tauri/src/main.rs
+++ diffly/src-tauri/src/main.rs
@@ -1,5 +1,5 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Prevents the Windows console host from opening for the desktop app.
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 
 fn main() {
   app_lib::run();
