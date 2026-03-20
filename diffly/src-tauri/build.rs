1f5fe23aef491858bfe46693f76c47d89dc7c3c9
silentfrontier <267770863+silentfrontier@users.noreply.github.com>
2026-03-20 15:29:50 +0100

    Track icon changes in Tauri build

--- diffly/src-tauri/build.rs
+++ diffly/src-tauri/build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    println!("cargo:rerun-if-changed=icons");
+    println!("cargo:rerun-if-changed=tauri.conf.json");
+    println!("cargo:rerun-if-changed=tauri.build.conf.json");
     tauri_build::build()
 }
