5855e3482a3cb1270e1a031d4ed9ac93c0dd5e80
silentfrontier <267770863+silentfrontier@users.noreply.github.com>
2026-03-23 09:11:48 +0100

    Speed up debug builds

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,9 @@
 fn main() {
-    println!("cargo:rerun-if-changed=icons");
+    println!("cargo:rerun-if-changed=icons/32x32.png");
+    println!("cargo:rerun-if-changed=icons/128x128.png");
+    println!("cargo:rerun-if-changed=icons/128x128@2x.png");
+    println!("cargo:rerun-if-changed=icons/icon.icns");
+    println!("cargo:rerun-if-changed=icons/icon.ico");
     println!("cargo:rerun-if-changed=tauri.conf.json");
     println!("cargo:rerun-if-changed=tauri.build.conf.json");
     tauri_build::build()
