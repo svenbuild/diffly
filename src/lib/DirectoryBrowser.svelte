0e5e56a4b49364d32307898ff1567082cd86e508
silentfrontier <267770863+silentfrontier@users.noreply.github.com>
2026-03-25 18:17:11 +0100

    Align sidebar file rows on a shared grid

--- src/lib/DirectoryBrowser.svelte
+++ src/lib/DirectoryBrowser.svelte
@@ -135,7 +135,7 @@
                 <button
                   class:selected={selectedRelativePath === entry.relativePath}
                   class="file-row"
-                  style={`padding-left: ${group.depth * 14 + 12}px`}
+                  style={`--file-row-indent: ${group.depth * 14}px`}
                   type="button"
                   on:click={() => selectEntry(entry)}
                 >
